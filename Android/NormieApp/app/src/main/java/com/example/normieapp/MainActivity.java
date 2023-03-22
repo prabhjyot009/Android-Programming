589a96bf5899e8deb7e06c659865cfca0c97a64e
prabhjyot009 <prabh156dps@gmail.com>
2023-03-23 00:17:50 +0530

    app

---
+++ Android/NormieApp/app/src/main/java/com/example/normieapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.normieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    Button btn;
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name);
+        btn=findViewById(R.id.btn);
+        result=findViewById(R.id.result);
+        print_name(name,btn,result);
+    }
+    public static void print_name(EditText name,Button btn,TextView result)
+    {
+        btn.setOnClickListener(v -> {
+
+            String s1=name.getText().toString();
+            result.setText(s1);
+        });
+}
+}
+
