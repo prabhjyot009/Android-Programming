3d61c67910e48e443e4ad24b3c2e32a0629409ae
prabhjyot009 <prabh156dps@gmail.com>
2023-05-19 22:22:57 +0530

    App

--- Android/ArrayList/app/src/main/java/com/example/arraylist/MainActivity.java
+++ Android/ArrayList/app/src/main/java/com/example/arraylist/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.arraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,20 +13,28 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
-    ArrayList<String> courses;
+    Button btn;
+    ArrayList<String> courses=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.textView);
-        courses=new ArrayList<>();
+        tv=findViewById(R.id.tv);
+        btn=findViewById(R.id.btn);
+
         courses.add("Java");
         courses.add("Python");
         courses.add("C++");
         courses.add("Android");
         courses.add("Machine Learning");
-        tv.setText(courses.toString());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(String course:courses){
+                    tv.append(course+"\n");
+                }
+            }
+        });
+
     }
 }
-
-//xml file using linear layout
--- Android/Term Work/TWQuesThree/app/src/main/java/com/example/twquesthree/MainActivity.java
+++ Android/Term Work/TWQuesThree/app/src/main/java/com/example/twquesthree/MainActivity.java
@@ -1,11 +1,5 @@
 package com.example.twquesthree;
-//4.	Create an application which displays three buttons named Red, Yellow, and Green. The background color of activity should be changed to appropriate color when user clock on any of these buttons using radio button.
-
-// Path: app\src\main\java\com\example\twquesthree\MainActivity.java
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -13,31 +7,22 @@ import android.widget.Button;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
     RadioButton red, yellow, green;
-    Button btn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         red = findViewById(R.id.red);
         yellow = findViewById(R.id.yellow);
         green = findViewById(R.id.green);
-        btn = findViewById(R.id.btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (red.isChecked()) {
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                } else if (yellow.isChecked()) {
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                } else if (green.isChecked()) {
-                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
-                }
-            }
-        });
+    }
+    public void Red(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.RED);
+    }
+    public void Yellow(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+    }
+    public void green(View view) {
+        getWindow().getDecorView().setBackgroundColor(Color.GREEN);
     }
 }
