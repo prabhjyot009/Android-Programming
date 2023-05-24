d2270bd1440c65b4a9a194cd7c542d47fc74f60f
prabhjyot009 <prabh156dps@gmail.com>
2023-05-24 09:36:07 +0530

    App

--- Android/MenuBarDemo/app/src/main/java/com/example/menubardemo/ContextMenuBar.java
+++ Android/MenuBarDemo/app/src/main/java/com/example/menubardemo/ContextMenuBar.java
@@ -3,6 +3,7 @@ package com.example.menubardemo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -13,26 +14,44 @@ import android.widget.Toast;
 
 public class ContextMenuBar extends AppCompatActivity {
     ListView contactslist;
-    String[] contacts={"Deepak","Sameer","Prabhjyot","Khalid"};
+    String[] contacts={"Prabhjyot","Khalid","Ayush"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_context_menu_bar);
-        contactslist=(ListView) findViewById(R.id.contactslist);
+        contactslist=findViewById(R.id.contactslist);
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,contacts);
         contactslist.setAdapter(adapter);
         registerForContextMenu(contactslist);
+        contactslist.setOnItemClickListener((parent, view, position, id) -> {
+            /*String data=(String)parent.getItemAtPosition(position);
+            Toast.makeText(ContextMenuBar.this,data, Toast.LENGTH_SHORT).show();*/
+            switch (position){
+                case 0:
+                    Intent intent=new Intent(ContextMenuBar.this,MainActivity1.class);
+                    startActivity(intent);
+                    break;
+                case 1:
+                    Intent intent1=new Intent(ContextMenuBar.this,MainActivity2.class);
+                    startActivity(intent1);
+                    break;
+                case 2:
+                    Intent intent2=new Intent(ContextMenuBar.this,MainActivity3.class);
+                    startActivity(intent2);
+                    break;
+
+            }
+        });
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.context_menubar,menu);
-    }
-
-        @Override
+//    @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        getMenuInflater().inflate(R.menu.context_menubar,menu);
+//    }
+/*      /*  @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         int id=item.getItemId();
         if(id==R.id.call){
@@ -46,5 +65,5 @@ public class ContextMenuBar extends AppCompatActivity {
         else {
             return super.onContextItemSelected(item);
         }
-    }
+    }*/
 }
