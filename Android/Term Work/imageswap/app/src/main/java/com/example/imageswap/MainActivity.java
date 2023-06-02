2e733327c865f5b02c78603524839a96f78724ed
prabhjyot009 <prabh156dps@gmail.com>
2023-06-02 21:20:54 +0530

    tw

--- Android/Term Work/imageswap/app/src/main/java/com/example/imageswap/MainActivity.java
+++ Android/Term Work/imageswap/app/src/main/java/com/example/imageswap/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.imageswap;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView i1;
+    Button b1;
+    int i=0;
+    int[] image ={R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Made By Demon", Toast.LENGTH_SHORT).show();
+
+        b1=findViewById(R.id.b1);
+        i1=findViewById(R.id.i1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i1.setImageResource(image[i]);
+                i++;
+                if (i==5)
+                    i=0;
+
+            }
+        });
+
+
+
     }
 }
