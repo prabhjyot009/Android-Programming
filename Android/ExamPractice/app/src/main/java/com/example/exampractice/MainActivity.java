2818e22c78c2b79d22a8a523600510d8040dc7c8
prabhjyot009 <prabh156dps@gmail.com>
2023-06-13 16:51:58 +0530

    App

--- Android/ExamPractice/app/src/main/java/com/example/exampractice/MainActivity.java
+++ Android/ExamPractice/app/src/main/java/com/example/exampractice/MainActivity.java
@@ -2,51 +2,40 @@ package com.example.exampractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText et1;
-    Button b1;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email;
+    Button send;
+
     TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et1=(EditText) findViewById(R.id.et1);
-        b1=(Button) findViewById(R.id.b1);
-        tv1=(TextView) findViewById(R.id.tv1);
-        b1.setOnClickListener(this);
+        send=findViewById(R.id.send);
+        email=findViewById(R.id.email);
+        Random random=new Random();
+        int min = 10000;
+        int max = 99999;
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        send.setOnClickListener(view -> {
+            String s3=email.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{s3});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Random Number");
+            intent.putExtra(Intent.EXTRA_TEXT,""+randomNumber);
+            intent.setType("message/rfc822");
 
-    }
-    public static boolean isStringUpperCase(String str){
-        char[] charArray=str.toCharArray();
-        for(int i=0;i<charArray.length;i++){
-            if(!Character.isUpperCase(charArray[i]))
-                return false;
-            }
-            return true;
-    }
-    @Override
-    public void onClick(View view) {
-        et1.setTextColor(Color.RED);
-        et1.setHintTextColor(Color.RED);
-
-        String s1= et1.getText().toString();
-        if(isStringUpperCase(s1)){
-            String ss=s1.toLowerCase();
-            et1.setTextColor(Color.RED);
-            et1.setText(""+ss);
-        }
-        if(!(isStringUpperCase(s1))){
-            String dd=s1.toUpperCase();
-            et1.setTextColor(Color.RED);
-            et1.setText(""+dd);
-        }
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+        });
     }
 }
