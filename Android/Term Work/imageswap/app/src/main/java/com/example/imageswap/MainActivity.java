dca0d2a7a391b73033e95734d9ef2cc8dc8c24ca
prabhjyot009 <prabh156dps@gmail.com>
2023-06-25 16:19:02 +0530

    app

--- Android/Term Work/imageswap/app/src/main/java/com/example/imageswap/MainActivity.java
+++ Android/Term Work/imageswap/app/src/main/java/com/example/imageswap/MainActivity.java
@@ -1,40 +1,3 @@
-/*package com.example.imageswap;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-    ImageView i1;
-    Button b1;
-    int i=0;
-    int[] image ={R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5};
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1=findViewById(R.id.b1);
-        i1=findViewById(R.id.i1);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i1.setImageResource(image[i]);
-                i++;
-                if (i==5)
-                    i=0;
-
-            }
-        });
-
-
-
-    }
-}
-#increase image size on click of button 5 times
-// Path: app\src\main\java\com\example\imageswap\MainActivity.java*/
 package com.example.imageswap;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -66,14 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        i1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i1.getLayoutParams().height=500;
-                i1.getLayoutParams().width=500;
-                i1.requestLayout();
-            }
-        });
+
 
     }
 }
